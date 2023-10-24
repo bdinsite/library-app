0b482701ffd254f80a533eaa2b57f689b711c699
bdinsite <bdinsight.oy@gmail.com>
2023-10-24 10:57:14 +0300

    Add books object to js file - initial library

---
+++ src/js/scripts.js
@@ -0,0 +1,84 @@
+// LIBRARY - LIST OF BOOKS
+const books = [
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      publishYear: 1925,
+      pages: 180,
+      category: "Classic",
+      availability: "available"
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      publishYear: 1960,
+      pages: 281,
+      category: "Fiction",
+      availability: "available"
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      publishYear: 1949,
+      pages: 328,
+      category: "Dystopian",
+      availability: "available"
+    },
+    {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      publishYear: 1937,
+      pages: 310,
+      category: "Fantasy",
+      availability: "available"
+    },
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      publishYear: 1951,
+      pages: 224,
+      category: "Coming of Age",
+      availability: "available"
+    },
+    {
+        title: "Last Summer",
+        author: "Jane Tyle",
+        publishYear: 2020,
+        pages: 300,
+        category: "Romance",
+        availability: "available"
+    },
+    {
+        title: "How to Lose Millions",
+        author: "Gene Ious",
+        publishYear: 2015,
+        pages: 250,
+        category: "Non-fiction",
+        availability: "available"
+    },
+    {
+        title: "My First Tooth",
+        author: "Paul Reed",
+        publishYear: 2005,
+        pages: 578,
+        category: "Non-fiction",
+        availability: "available"
+    },
+    {
+        title: "How to Find a Girl When You're Broke",
+        author: "Kenneth Lint",
+        publishYear: 2022,
+        pages: 2,
+        category: "Non-fiction",
+        availability: "available"
+    },
+    {
+        title: "Just Found Dust Under the Bed",
+        author: "Karen Nitpick",
+        publishYear: 1988,
+        pages: 895,
+        category: "Horror",
+        availability: "available"
+    }
+
+];
