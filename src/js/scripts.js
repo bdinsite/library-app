a08f816852b1f1190ebfaaa9b18de355ca3f4311
bdinsite <bdinsight.oy@gmail.com>
2023-10-24 11:28:39 +0300

    Add search engine script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -82,3 +82,241 @@ const books = [
     }
 
 ];
+
+let search = [];
+let result = [];
+
+let title = document.querySelector('#title');
+let author = document.querySelector('#author');
+let category = document.querySelector('#category');
+let publishYear = document.querySelector('#publishYear');
+let pages = document.querySelector('#pages');
+let btn = document.querySelector('#btn');
+let reset = document.querySelector('#reset');
+let ol = document.querySelector('ol');
+let li = document.querySelector('li');
+let input = document.querySelectorAll('input');
+
+btn.addEventListener('click', searchBook);
+reset.addEventListener('click', resetForm);
+
+// RESET FORM AND RESULT FIELD
+function resetForm(){
+    window.location.reload();
+    input.forEach((input) => {
+        input.value = "";
+    })
+}
+
+// ADD NULL TO FIELDS THAT AREN'T FILLED IN, AS "" WILL MEET ALL CRITERIA IN "includes()"
+function searchBook(){
+    search = []; // Reset search array
+    result = []; // Reset search result object
+    
+    if(ol.hasChildNodes()){ol.removeChild(ol.children[0])} // Remove list in case of search re-run so that result list gets cleared in between
+    if(title.value == ""){search.push(null)} else{search.push(title.value)};
+    if(author.value == ""){search.push(null)} else{search.push(author.value)};
+    if(category.value == ""){search.push(null)} else{search.push(category.value)};
+    search.push(Number(publishYear.value));
+    search.push(Number(pages.value));
+    console.log(search);
+
+    for (let i = 0; i < books.length; i++){
+        let searchResult = document.querySelector('.bookList');
+        let li = document.createElement('li');
+        let h4 = document.createElement('h4');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let p5 = document.createElement('p');
+        let button = document.createElement('button');
+        // button.id = 'borrow';
+
+        if(search[0] != null && books[i].title.toLowerCase().includes(search[0].toLowerCase())){
+            h4.textContent = "Title: " + books[i].title;
+            p1.textContent = "Author: " + books[i].author;
+            p2.textContent = "Category: " + books[i].category;
+            p3.textContent = "Published: " + books[i].publishYear;
+            p4.textContent = "Pages: " + books[i].pages;
+            p5.textContent = "Availability: " + books[i].availability;
+            button.textContent = "Borrow";
+            button.classList.add(i); // Add book index number in button -class (to use when clicking borrow-button)
+            if(books[i].availability !== "available"){
+                button.disabled = true;
+                button.style.backgroundColor = "#f4f4f5";
+                p5.style.color = "red";
+            } else {
+                p5.style.color = "green";
+            }
+            li.appendChild(h4);
+            li.appendChild(p1);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            li.appendChild(p4);
+            li.appendChild(p5);
+            li.appendChild(button);
+
+            searchResult.appendChild(li);
+
+            result.push(books[i]);
+
+            // Grab booklist index via "Borrow" -button
+            button.addEventListener('click', addBooks);
+
+            function addBooks(evt){
+                console.log(evt.target.classList.value);
+            }
+
+
+        } else if(search[1] != null && books[i].author.toLowerCase().includes(search[1].toLowerCase())){
+            h4.textContent = "Title: " + books[i].title;
+            p1.textContent = "Author: " + books[i].author;
+            p2.textContent = "Category: " + books[i].category;
+            p3.textContent = "Published: " + books[i].publishYear;
+            p4.textContent = "Pages: " + books[i].pages;
+            p5.textContent = "Availability: " + books[i].availability;
+            button.textContent = "Borrow";
+            button.classList.add(i); // Add book index number in button -class (to use when clicking borrow-button)
+            if(books[i].availability !== "available"){
+                button.disabled = true;
+                button.style.backgroundColor = "#f4f4f5";
+                p5.style.color = "red";
+            } else {
+                p5.style.color = "green";
+            }
+            li.appendChild(h4);
+            li.appendChild(p1);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            li.appendChild(p4);
+            li.appendChild(p5);
+            li.appendChild(button);
+
+            searchResult.appendChild(li);
+
+            result.push(books[i]);
+
+            // Grab booklist index via "Borrow" -button
+            button.addEventListener('click', addBooks);
+
+            function addBooks(evt){
+                console.log(evt.target.classList.value);
+            }
+        } else if(search[2] != null && books[i].category.toLowerCase().includes(search[2].toLowerCase())){
+            h4.textContent = "Title: " + books[i].title;
+            p1.textContent = "Author: " + books[i].author;
+            p2.textContent = "Category: " + books[i].category;
+            p3.textContent = "Published: " + books[i].publishYear;
+            p4.textContent = "Pages: " + books[i].pages;
+            p5.textContent = "Availability: " + books[i].availability;
+            button.textContent = "Borrow";
+            button.classList.add(i); // Add book index number in button -class (to use when clicking borrow-button)
+            if(books[i].availability !== "available"){
+                button.disabled = true;
+                button.style.backgroundColor = "#f4f4f5";
+                p5.style.color = "red";
+            } else {
+                p5.style.color = "green";
+            }
+            li.appendChild(h4);
+            li.appendChild(p1);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            li.appendChild(p4);
+            li.appendChild(p5);
+            li.appendChild(button);
+
+            searchResult.appendChild(li);
+
+            result.push(books[i]);
+
+            // Grab booklist index via "Borrow" -button
+            button.addEventListener('click', addBooks);
+
+            function addBooks(evt){
+                console.log(evt.target.classList.value);
+            }
+        } else if(books[i].publishYear === search[3]){
+            h4.textContent = "Title: " + books[i].title;
+            p1.textContent = "Author: " + books[i].author;
+            p2.textContent = "Category: " + books[i].category;
+            p3.textContent = "Published: " + books[i].publishYear;
+            p4.textContent = "Pages: " + books[i].pages;
+            p5.textContent = "Availability: " + books[i].availability;
+            button.textContent = "Borrow";
+            button.classList.add(i); // Add book index number in button -class (to use when clicking borrow-button)
+            if(books[i].availability !== "available"){
+                button.disabled = true;
+                button.style.backgroundColor = "#f4f4f5";
+                p5.style.color = "red";
+            } else {
+                p5.style.color = "green";
+            }
+            li.appendChild(h4);
+            li.appendChild(p1);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            li.appendChild(p4);
+            li.appendChild(p5);
+            li.appendChild(button);
+
+            searchResult.appendChild(li);
+
+            result.push(books[i]);
+             // Grab booklist index via "Borrow" -button
+             button.addEventListener('click', addBooks);
+
+             function addBooks(evt){
+                 console.log(evt.target.classList.value);
+             }
+        } else if(books[i].pages === search[4]){
+            h4.textContent = "Title: " + books[i].title;
+            p1.textContent = "Author: " + books[i].author;
+            p2.textContent = "Category: " + books[i].category;
+            p3.textContent = "Published: " + books[i].publishYear;
+            p4.textContent = "Pages: " + books[i].pages;
+            p5.textContent = "Availability: " + books[i].availability;
+            button.textContent = "Borrow";
+            button.classList.add(i); // Add book index number in button -class (to use when clicking borrow-button)
+            if(books[i].availability !== "available"){
+                button.disabled = true;
+                button.style.backgroundColor = "#f4f4f5";
+                p5.style.color = "red";
+            } else {
+                p5.style.color = "green";
+            }
+            li.appendChild(h4);
+            li.appendChild(p1);
+            li.appendChild(p2);
+            li.appendChild(p3);
+            li.appendChild(p4);
+            li.appendChild(p5);
+            li.appendChild(button);
+
+            searchResult.appendChild(li);
+
+            result.push(books[i]);
+
+            // Grab booklist index via "Borrow" -button
+            button.addEventListener('click', addBooks);
+
+            function addBooks(evt){
+                console.log(evt.target.classList.value);
+            }
+        }
+    } console.log(result);
+}
+
+
+// LIST OF BOOKS BORROWED
+function Book(title, author, category, publishYear, pages){
+    this.title = title;
+    this.author = author;
+    this.category = category;
+    this.publishYear = publishYear;
+    this.pages = pages;
+    this.info = function(){
+        return `${title} by ${author} - a ${category} Novel`;
+    }
+}
